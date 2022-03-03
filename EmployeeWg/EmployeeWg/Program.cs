ee3b11f61457642d8525549d8ead64e132742b3f
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-03 14:14:31 +0530

    Add file for Attendence

--- EmployeeWg/EmployeeWg/Program.cs
+++ EmployeeWg/EmployeeWg/Program.cs
@@ -8,6 +8,10 @@ namespace EmployeeWg
         static void Main(string[] args)
         {
             Console.WriteLine("Welcom to the Employee Wage Problem");
+
+            EmpWage employee = new EmpWage();
+            employee.EmployeeAttendance();
+
         }
     }
 }
