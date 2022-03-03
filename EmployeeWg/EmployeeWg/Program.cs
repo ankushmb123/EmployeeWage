4ca94b846667faa172a2738fa8c7cdebe93e0fc7
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-03 14:07:10 +0530

    Initial Commit

---
+++ EmployeeWg/EmployeeWg/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWg
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to the Employee Wage Problem");
+        }
+    }
+}
